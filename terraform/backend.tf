b4464e447b573b2a4cc3a5d7a7e946a5fe6be9c9
Abdul Shaikh <abdulshaikhr99@gmail.com>
2025-10-26 19:23:25 -0400

    -

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backend-cloud-cost-optimizer"    
+    key            = "envs/staging/terraform.tfstate" 
+    region         = "us-east-1"
+    dynamodb_table = "terraform-backend-cloud-cost-optimizer"
+    encrypt        = true
+  }
+}
