40d5f93c46f7efe0db429266f3e4cae687cff118
Abdul Shaikh <abdulshaikhr99@gmail.com>
2025-10-26 18:16:17 -0400

    test

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -30,3 +30,45 @@ resource "aws_iam_role_policy" "lambda_bedrock_policy" {
   })
 }
 
+resource "aws_iam_role_policy" "lambda_s3_access" {
+  name = "lambda-s3-access"
+  role = aws_iam_role.lambda_execution_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "arn:aws:s3:::levelup-reports-staging/*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "lambda_exec_staging_policy" {
+  name = "lambda-exec-staging-policy"
+  role = aws_iam_role.lambda_execution_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "dynamodb:*",
+          "s3:*",
+          "sns:Publish" 
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
